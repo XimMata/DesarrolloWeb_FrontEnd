18242bc9a1daca58b48d434edc19edd8c8e4bbbf
XimMata <ximena.g.m.m@gmail.com>
2024-10-06 18:25:19 -0600

    09_Operador_Ternario

---
+++ introjs/src/09_Operador_Ternario.js
@@ -0,0 +1,20 @@
+//Ejemplo 1
+console.log("Obtiene aprobado y reprobado, donde el promedio es 7.0");
+const promedio = 7.0;
+
+let resultado ='';
+resultado=(promedio>=7.0)? 'Aprobado' : 'Reprobado'
+
+console.log("El resultado es: ", resultado);
+
+//Ejemplo 2
+console.log("Encuentra el valor maximo");
+let valorMaximo=0;
+const a= 4;
+const b= 6;
+const c=10;
+
+valorMaximo=a>b?a:b;
+valorMaximo=valorMaximo>c?valorMaximo:c;
+
+console.log("El valor maximo es: ", valorMaximo);
